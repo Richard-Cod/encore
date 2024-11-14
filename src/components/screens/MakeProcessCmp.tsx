fcd1ebcea1815504a117b52b8c7a65db7902b64f
Guillaumek83 <guillaume@eumlet.com>
2024-11-14 23:04:01 +0400

    Update MakeProcessCmp.tsx

--- src/components/screens/MakeProcessCmp.tsx
+++ src/components/screens/MakeProcessCmp.tsx
@@ -139,12 +139,12 @@ const MakeProcessCmp = () => {
 
 
 
-        setisFormSubmitted(false);
-        formik.setValues({ email: "", companyName: "", country: "" });
-        setselectedCategory(null);
-        setselectedSubCategory(null);
-        formik.setErrors({ companyName: "", country: "", email: "" });
-        formik.setTouched({ companyName: false, country: false, email: false });
+        //setisFormSubmitted(false);
+        //formik.setValues({ email: "", companyName: "", country: "" });
+        //setselectedCategory(null);
+        //setselectedSubCategory(null);
+        //formik.setErrors({ companyName: "", country: "", email: "" });
+        //formik.setTouched({ companyName: false, country: false, email: false });
 
         setTimeout(() => {
           listUserAccounts({
